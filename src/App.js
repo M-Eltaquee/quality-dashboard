06d6ff3b024bc08c14bf0e82bb592cd2edc74f1f
Mohamed Osman <igs.meltaquee@gmail.com>
2022-08-08 21:08:20 +0400

    create comming soon page

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Wah Gwan, Soon Come </h1>
+      <p>What's Up, Comming Soon</p>
+    </div>
+  );
+}
+
+export default App;
